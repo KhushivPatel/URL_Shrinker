1d99f22070306c818c5f93d922541db5ddaaa49a
Khushi Patel <khushi2002806@gmail.com>
2023-09-12 00:43:16 +0530

    new style

--- scr.js
+++ scr.js
@@ -12,3 +12,48 @@ async function shorten(){
    console.log(data.result.short_link2)
 }
 shorten()
+
+document.getElementById("shorted").addEventListener("click", ()=>{
+
+    const tooltip = document.getElementById("tooltip");
+    tooltip.style.display = "block"
+    // Hide the tooltip after 1 second
+    setTimeout(() => {
+        tooltip.style.display = "none"
+    }, 500); // 1000 milliseconds (1 second)
+})
+  
+  
+
+
+
+// let copyText=document.getElementById("shorted");
+// copyText.addEventListener("click",function(){
+    // const cssRule = `#shorted.active::before{
+    //     display:none;
+    // }
+    // #shorted.active::after{
+    //     display:none;
+    // }
+    // `;
+    // const styleElement = document.createElement("style");
+    // styleElement.appendChild(document.createTextNode(cssRule));
+    // document.head.appendChild(styleElement);
+
+
+
+    // setTimeout(()=>{
+    //     console.log("jhdf");
+    //     document.head.removeChild(styleElement);
+    // },500)
+// })
+
+
+
+
+// copyText.querySelector("button").addEventListener("click",function(){
+//     let input=copyText.querySelector("input.text");
+//     input.select();
+//     document.execCommand("copy");
+//     copyText.classList.add("active");
+// })
